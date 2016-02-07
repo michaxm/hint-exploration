034c421d925be1ed2d694c42fc6b554058a2c51e
Axel Mannhardt <notset@example.com>
2016-02-07 11:30:31 +0100

    testing parallel

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,31 @@
 module Main where
 
+import Control.Concurrent.Async
 import qualified Data.ByteString.Lazy.Char8 as BLC
 import Language.Haskell.Interpreter
 
 main :: IO ()
-main = do r <- runInterpreter testHint
-          case r of
-            Left err -> printInterpreterError err
-            Right () -> putStrLn "that's all folks"
+main = do
+  threads <- mapM (async . runSingle) ["qwe", "sdf", "xxx"]
+  mapM_ wait threads
+
+runSingle :: String -> IO ()
+runSingle input = do
+  r <- runInterpreter $ testHint input
+  case r of
+   Left err -> printInterpreterError err
+   Right () -> putStrLn "that's all folks"
 
 -- observe that Interpreter () is an alias for InterpreterT IO ()
-testHint :: Interpreter ()
-testHint =
-    do
-      loadModules ["resources/Module.hs"]
-      setTopLevelModules ["Module"]
-      setImportsQ [("Prelude", Nothing), ("Data.ByteString.Lazy.Char8", Just "BLC")]
-      f <- interpret "f" (as :: BLC.ByteString -> BLC.ByteString)
-      res <- return (f (BLC.pack "qwe") :: BLC.ByteString)
-      say $ show $ res
+testHint :: String -> Interpreter ()
+testHint input =
+  do
+    loadModules ["resources/Module.hs"]
+    setTopLevelModules ["Module"]
+    setImportsQ [("Prelude", Nothing), ("Data.ByteString.Lazy.Char8", Just "BLC")]
+    f <- interpret "f" (as :: BLC.ByteString -> BLC.ByteString)
+    res <- return (f (BLC.pack input) :: BLC.ByteString)
+    say $ show $ res
       
 say :: String -> Interpreter ()
 say = liftIO . putStrLn
